efda768e7165c1145153a346c266f360210c3492
Mikhail_Pershin <mcpershin@yandex.ru>
2023-03-19 16:57:33 +0300

    add solution of task 21

--- Program.cs
+++ Program.cs
@@ -8,22 +8,56 @@
 
 // 23432 -> да
 
-Console.Write("Введите число: ");
-string number = Console.ReadLine()!;
-int len = number.Length;
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// int len = number.Length;
 
-if (len == 5)
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"Число {number} является палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число {number} является не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number} не является пятизначным");
+// }
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+int ReadInt(string text)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"Число {number} является палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"Число {number} является не палиндром");
-    }
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
 }
-else
+
+double Distance(int[] a, int[] b)
 {
-    Console.WriteLine($"Число {number} не является пятизначным");
+return Math.Round(Math.Sqrt(Math.Pow(a[0] - b[0], 2) + Math.Pow(a[1] - b[1], 2) + Math.Pow(a[2] - b[2], 2)), 2);
 }
+
+int[] a = new int[3];
+int[] b = new int[3];
+
+a[0] = ReadInt("Введите координату X точки A: ");
+a[1] = ReadInt("Введите координату Y точки A: ");
+a[2] = ReadInt("Введите координату Z точки A: ");
+b[0] = ReadInt("Введите координату X точки B: ");
+b[1] = ReadInt("Введите координату Y точки B: ");
+b[2] = ReadInt("Введите координату Z точки B: ");
+double res = Distance(a, b);
+
+System.Console.WriteLine("Расстояние между точками = " + res);
